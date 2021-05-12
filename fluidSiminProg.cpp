ec52ef3730fb99e359623a6a5d07c18b56fc6399
Paul Schulze <paul@paulschulze.net>
2021-05-12 15:04:44 -0500

    First prototype of "vorticity" function

--- fluidSiminProg.cpp
+++ fluidSiminProg.cpp
@@ -1,12 +1,16 @@
 #define dt 0.15
 //radius
 #define r
-vec2 advect(sampler2D prevField, vec2 uv, vec4 curMouse, vec4 prevMouse){
+
+//user-defined per
+
+
+Vec2 advect(sampler2D prevField, vec2 uv, vec4 curMouse, vec4 prevMouse){
     //eq 13
     vec2 uCur = textureLod(prevField,uv,0.).xyz;
     vec2 posPrev = uv - dt*uCur;
     return textureLod(prevField,posPrev,0.);
-    
+
 }
 //rBeta is the reciprocal of beta
 vec2 jacobi(sampler2D field,int alpha,int rbeta,int b,vec2 stepSize){
@@ -20,15 +24,38 @@ vec2 jacobi(sampler2D field,int alpha,int rbeta,int b,vec2 stepSize){
 vec2 addForce(sampler2D field, vec2 uv, vec2 posForce){
     vec3 uCur = textureLod(field,uv,0.).xyz;
     return uCur + exp(distance(posForce,uCur)/r);
-    
-    
+
+
 }
 vec2 divergence(sampler2D field, vec2 uv,vec2 stepSize){
 
 }
 vec2 solvePoissonPressure(sampler2D field, vec2 uv){
-    //calculate jacobian of pressure 
+    //calculate jacobian of pressure
     //jacobi method (30 iterations)
     //take result and subtract gradient of w
     //return that result
+}
+
+double vorticity (sampler2D velocityField, vec2 pos, vec2 stepSize) {
+  //returns double vorticity at point
+  //Technically vorticity is a 3d vector, but in this 2d setting it will
+  //Always be (0, 0, z) for some z, so we just use that z
+
+  //dFy_dx is (dF_y)/(dx) [i.e. an approximation of it]
+  double dFy_dx = (textureLod(velocityField, pos + vec2(stepSize.x, 0.), 0.).y -
+        textureLod(velocityField, pos + vec2(stepSize.x, 0.), 0.).y) /
+      (2*stepSize.x)
+
+  //Similar for dFx_dy
+  double dFx_dy = (textureLod(velocityField, pos + vec2(0, stepSize.y), 0.).x -
+        textureLod(velocityField, pos + vec2(0, stepSize.y), 0.).x) /
+      (2*stepSize.y)
+
+  //Return vorticity
+  return dFy_dx - dFx_dy;
+}
+
+vec2 vorticityConfinement (sampler2D velocityField, vec2 vorticity, vec2 spacing, vec2 pos){
+  //returns vec2 corrective force from "vorticity confinement"
 }
